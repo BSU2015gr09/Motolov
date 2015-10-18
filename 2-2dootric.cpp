a8f158db09607aa0b2082eb25d104baf71661df3
lol <zhenia@gmail.com>
2015-10-18 21:57:44 +0300

    second homework

---
+++ 2-2dootric.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "Russian");
+	int num = 0, sum = 0;
+	do{
+		cout << "введите число" << "\n";
+		cin >> num;
+		if (num > 0){
+			sum = sum + num;
+		}
+	} while (num > 0); 
+	cout << "—умма положительных чисел равана:" << sum;
+	return 0;
+}
---
+++ 2-3naibcifra.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "Russian");
+	int num = 0, s = 0, i = 1, max = 0,q=0,num1=0;
+	cin >> num;
+	num1 = num;
+	while (num > 0){
+		
+		s = num % 10;
+		if (max < s){
+			max = s;
+			
+        }
+		
+		num = num / 10;
+	}
+	cout << "Наибольшей цифрой данного числа является:" << max << "\n" << "данная цифра находится на " << " позиции слева: ";
+	while (num1 > 0){
+		q = num1 % 10;
+		if (q == max){
+			cout << i << ",";
+		}
+		i++;
+		num1 = num1 / 10;
+	}
+	
+	
+}
