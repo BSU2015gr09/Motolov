8affde6f61e79dc154fa1ff0698a5f79e9a41987
lol <zhenia@gmail.com>
2015-10-26 04:54:35 +0300

    add homework

---
+++ 3bonustask.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int a=0, b=0, c=0, d=0,e=0,g=0,f=0,h=0, i=1,q=9,num;
+	cin >> num;
+	e = num;
+	while (num > 0){
+
+		a = e % 10;
+		e = e / 10;
+		b = b + a;
+	}
+	cout << "begin" << endl;
+	
+	if (num < 10){
+		
+		q++;
+		while (q>0){
+			g = q;
+			f = g % 10;
+			g = g / 10;
+			h = h + f;
+			if (h = b){
+				cout  << q;
+			}
+		}
+	}else
+	while (b != d){
+		
+		i++;
+		c = i % 10;
+		d = d + c;
+	}
+	cout << i;
+
+}
---
+++ 3fibonachi.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <clocale>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int i, q = 0;
+	double array[40], pr = 0, ne = 0;
+	array[0] = 0;
+	array[1] = 1;
+	cout << "   |                           ќтношение членов ‘ибоначчи                |" << endl;
+	cout << "     |последующее к предыдущему| предыдущее к последующему|" << endl;
+	//cout << "|" << i << ":" << q << "___________________________________|" << q << ":" << i << "___________________________________|" << endl;
+	for (i = 2; i<40; i++)
+	{
+		q++;
+		array[i] = array[i - 1] + array[i - 2];
+		pr = array[i] / array[i - 1];
+		ne = array[i - 1] / array[i];
+		cout  << i << ":" << q ;
+		cout << setw(6) << "|";
+		cout << setw(20) << setprecision(4) << pr;
+		cout << setw(5) << "|";
+		cout << setw(4) << fixed << setprecision(4) << " " << ne << "|" << q << ":" << i << endl;
+	}
+	cout << "   |__________________________|_____________________________|" << endl;
+	system("pause");
+}
