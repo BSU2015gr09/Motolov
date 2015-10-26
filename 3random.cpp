10ae96c92b56209ba8ad9cad30453ec425c38b86
barvenov <barvenovsa@gmail.com>
2015-10-26 22:16:29 +0300

    Недочеты в программе

--- 3random.cpp
+++ 3random.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	long int a = 4096, c = 150889, m = 714025;
 	long int i = 1;
-	int mas[10];
+	int mas[10]; // Зачем массив?????
 	mas[0] = 1;
 	cout << " 1 : " << mas[0] << "\n";
 	while (i<11)
